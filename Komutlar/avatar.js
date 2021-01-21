b99525130c2f5d3b176031f876ec5c044d14617d
Chrome85 <75063366+Chrome85@users.noreply.github.com>
2021-01-21 14:40:22 +0300

    Add files via upload

--- Komutlar/avatar.js
+++ Komutlar/avatar.js
@@ -1,28 +1,42 @@
-const Discord = require('discord.js')//Modülümüzü tanımladık
-exports.run = async(client, message) => {//Komutu çalıştıracak olan yer
-let user = message.mentions.users.first()//Kullanıcı belirttik eğer kullanıcı etiketlersek onun avatarını eğer etiketlemez isek bizim avatarımızı göterece
-if(user) {//eğer kullanıcı etiketlersek bunu gnderecek
-const embed = new Discord.MessageEmbed()//Artık richembed yerine MessageEmbed() oldu bilginize
-.setTitle(`<a:satik:739390491853979710>Kullanıcının Avatarı`)//başlık
-.setImage(user.avatarURL())//resimi gönderecek
-.setColor(`BLACK`)//embed rengini ayarladık
-message.channel.send(embed)//kanala mesajı gönderiyoruz
-} else {//eğer etiketlemez isek bunu
-const embed = new Discord.MessageEmbed()//Artık richembed yerine MessageEmbed() oldu bilginize
-.setTitle(`<a:satik:739390491853979710>Kullanıcının Avatarı`)//başlık
-.setImage(message.author.displayAvatarURL())//resimi gönderecek
-.setColor(`BLACK`)//embed rengini ayarladık
-message.channel.send(embed)//kanala mesajı gönderiyoruz
-}
+const Discord = require('discord.js');
+const db = require("wio.db")
+exports.run = async (client, message, args) => {
+  try {
+  let user;
+  if (message.mentions.users.first()) {
+    user = message.mentions.users.first();
+  } else {
+    user = message.author;
+  }
+
+  message.channel.send({ embed: {
+    author: {
+      name: user.tag
+    },
+    color: "#4B0082",
+    description: `[\`WEBP\`](${user.displayAvatarURL({ format: 'webp', dynamic: true, size:4096 })})・[\`PNG\`](${user.displayAvatarURL({ format: 'png', dynamic: true, size:4096 })})・[\`JPG\`](${user.displayAvatarURL({ format: 'jpg', dynamic: true, size:4096 })})`,
+    image: {
+      url: user.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 })
+    },
+    footer: {
+      text: "Gloss Bot",
+      icon_url: client.user.avatarURL({ format: 'png' })
+    },
+    
+    timestamp: Date.now()
+  }})
+  } catch{}
 };
+
 exports.conf = {
-enabled: false,//kapalı?
-guildOnly: false, //sunucu özel?
-aliases: ['pp'], //diğer adlar
-permLevel: 0 //yetki
+  enabled: true, 
+  guildOnly: true, 
+  aliases: ["pp"],
+  permLevel: 0
 };
+
 exports.help = {
-name: 'avatar',//help komutu
-description: '',//açıklaması
-usage: ''//kullanımı
+  name: 'avatar', 
+  description: '',
+  usage: ''
 };
